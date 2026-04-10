312184af8d069a007ae20540724cd5eaff0f997b
MKJ <appp@N0-name.local>
2026-04-11 01:48:32 +0500

    remove Server code

--- Server/routes/progressRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getProgress, toggleLectureComplete } = require('../controllers/progressController');
-const { protect } = require('../middleware/authMiddleware');
-
-router.get('/:courseId', protect, getProgress);
-router.post('/:courseId/lecture/:lectureId', protect, toggleLectureComplete);
-
-module.exports = router;
